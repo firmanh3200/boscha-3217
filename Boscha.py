b8bebcc0b7b1e18c5185af49731bdb73d75f0558
firmanh3200 <135565052+firmanh3200@users.noreply.github.com>
2024-11-26 15:02:29 +0700

    Add files via upload

---
+++ Boscha.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+st.title(':orange[STATISTIK] :blue[DAERAH] :green[KABUPATEN BANDUNG BARAT]')
+st.subheader('', divider='rainbow')
+st.success('Aplikasi ini berisi kumpulan Indikator Makro yang resmi dirilis oleh Badan Pusat Statistik, \
+    ditambah data-data statistik sektoral yang sumbernya tercantum.')
+st.info('Aplikasi ini dibuat untuk memudahkan Para Pemangku Kepentingan dalam proses Perencanaan, Pelaksanaan, \
+    Monitoring dan Evaluasi Pembangunan di Kabupaten Bandung Barat.')
+st.warning('Silakan mengakses setiap indikator makro melalui menu di sebelah kiri.')
+
+st.subheader('', divider='rainbow')
+st.caption(':orange[Boscha - Statistik Daerah Kabupaten Bandung Barat]')
+st.caption('Hak Cipta @ :green[Badan Pusat Statistik Kabupaten Bandung Barat]')
